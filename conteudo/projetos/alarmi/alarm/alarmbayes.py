f2edb4a2e9d8309a731dbfd5209942b513eac783
Hendrik Macedo <hendrik@iMac-de-Hendrik.local>
2023-04-16 19:36:28 -0300

    alarmi wheel

--- conteudo/projetos/alarmi/alarm/alarmbayes.py
+++ conteudo/projetos/alarmi/alarm/alarmbayes.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from bayesian import *
+from bayesian.bayesian import *
 
 """# Exemplo do Alarme"""
 
---
+++ conteudo/projetos/alarmi/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='bayesian',
+    version='1.0',
+    packages=['.bayesian','.alarm'],
+)
